c6bd66dda2cd2f59bef9904cdcb1b810d15b3855
Renan Bonani <bonani.silva@gmail.com>
2021-06-04 23:26:34 -0300

    Update App.js

--- buscacep/src/App.js
+++ buscacep/src/App.js
@@ -9,23 +9,22 @@ export default () => {
     setCep( e.target.value );
   };
 
-  const getCEP = () =>{
-
+  const getCEP = async () =>{
+    let result = await fetch(`https://brasilapi.com.br/api/cep/v1/{${cep}}`);
+    let json = await result.json    
   }
 
-  return (
-    
-
-
+  return (  
     <div className="conteiner">
       <div className="form-grup">
         <h1>Busca de Cidades</h1>
         <h3 className="sub-titulo">Digite o CEP no campo abaixo.</h3>
         <form>
-            <input type="text" value={cep} onChange={handleInput} placeholder="Digite o seu CEP" />
+            <input type="text" value={cep} name="cep" onChange={handleInput} placeholder="Digite o seu CEP" />
             <h3>Não sabe o seu CEP ? <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="_blank">Pesquise aqui</a></h3>
             <input type="text"  disabled />
         </form>
+        <button onClick={getCEP}>Buscar</button>
       </div>        
     </div>
   );
