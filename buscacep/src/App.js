c616502867edf2f5d751eb6de0ec500e56afc207
Renan Bonani <bonani.silva@gmail.com>
2021-06-05 01:54:01 -0300

    organizando os codigos

--- buscacep/src/App.js
+++ buscacep/src/App.js
@@ -1,33 +1,50 @@
+import React from 'react';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Formik, Field, Form } from 'formik';
 
 
 export default () => {
-  const [cep, setCep] = useState('');
-  const handleInput = (e) =>{
-    setCep( e.target.value );
-  };
-  let cidade = useState('');
-  const getCEP = async () =>{
-    let result = await fetch(`https://brasilapi.com.br/api/cep/v1/{${cep}}`);
-    let json = await result.json
-    cidade =json.city;
-    console.log("Resultado: ",cidade);    
+  
+  function onSubmit(values, actions) {
+    console.log('SUBMIT', values);
   }
 
+  function onBlurCep(ev, setFieldValue) {
+    const { value } = ev.target;
 
-  return (  
-    <div className="conteiner">
-      <div className="form-grup">
-        <h1>Busca de Cidades</h1>
-        <h3 className="sub-titulo">Digite o CEP no campo abaixo.</h3>
-        <form>
-            <input type="text" value={cep} onChange={handleInput} onBlur={getCEP} placeholder="Digite o seu CEP"/>
+    const cep = value?.replace(/[^0-9]/g, '');
+
+    if (cep?.length !== 8) {
+      return;
+    }
+
+    fetch(`https://brasilapi.com.br/api/cep/v1/{${cep}}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFieldValue('cidade', data.city);
+      });
+    }
+
+
+  return ( 
+    <Formik
+      onSubmit={onSubmit}
+      validateOnMount
+      initialValues={{
+        cep: '',
+        cidade: '',
+      }}
+      render={({ setFieldValue }) => (    
+               
+        <Form className="conteiner">
+          <div className="form-grup">
+            <h1>Busca de Cidades</h1>
+            <h3 className="sub-titulo">Digite o CEP no campo abaixo.</h3>          
+            <Field name="cep" type="text" onBlur={(ev) => onBlurCep(ev, setFieldValue)} />
             <h3>Não sabe o seu CEP ? <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="_blank">Pesquise aqui</a></h3>
-            <input type="text" value={cidade} disabled />
-        </form>
-      </div>        
-    </div>
-  );
-}
-
+            <Field name="cidade" type="text" />
+          </div>         
+        </Form>
+      )}
+    />
+  )}  
